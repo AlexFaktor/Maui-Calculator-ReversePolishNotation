809108aba96e62091b64b78c4382c178e0d8f29c
Alex Faktor <118138704+AlexFaktor@users.noreply.github.com>
2023-12-03 17:01:39 +0200

    checkpoint

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,16 +2,39 @@
 {
     public class Program
     {
+        private static string ShowCalculateResult(string input)
+        {
+            try
+            {
+                return ExpressionCalculator.Calculate(input).ToString();
+            }
+            catch (ArgumentException ex)
+            {
+                return "Incorrect input format";
+            }
+            catch (DivideByZeroException ex)
+            {
+                return "Not divisible by 0";
+            }
+            catch (ArithmeticException ex)
+            {
+                return "Not all brackets are closed";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
         static void Main(string[] args)
         {
             if (args.Length != 0)
-            { 
+            {
 
             }
 
             Console.Write("Enter the expression -> ");
             var input = Console.ReadLine();
-            Console.WriteLine($"{input} -> {ExpressionCalculator.Calculate(input)}");
+            Console.WriteLine($"{input} -> {ShowCalculateResult(input)}");
         }
     }
 }
